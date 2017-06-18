023dcc4e1beaed64ad25bb5d4c83e620ddfb4ff3
huancung <huan.cung@gmail.com>
2017-06-17 23:28:05 -0700

    milestone commit

---
+++ Pokedex/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Pokedex
+//
+//  Created by Huan Cung on 6/15/17.
+//  Copyright © 2017 Huan Cung. All rights reserved.
+//
+
+import Foundation
---
+++ Pokedex/PokemonDetailsVC.swift
@@ -0,0 +1,35 @@
+//
+//  PokemonDetailsVC.swift
+//  Pokedex
+//
+//  Created by Huan Cung on 6/14/17.
+//  Copyright © 2017 Huan Cung. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
